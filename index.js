8011fc76a865e7243a834605c8b837ac0bb5b28c
Artem <borovenskyartem@gmail.com>
2018-05-03 22:29:30 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+// process.env.PORT --- will automatically defined by HEROKU 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
